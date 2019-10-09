befdb4005e5d294f779e7dc189ac300d68b620a1
androidlutz <55858485+androidlutz@users.noreply.github.com>
2019-10-08 18:58:59 -0700

    first upload

---
+++ src/ChapterOneExercises.java
@@ -0,0 +1,78 @@
+//HEADER
+//Program Name: Chapter 1 Exercises 
+//Author: Andrew Lutz
+//Class: CS160 Fall 2019
+//Date: 10/4/2019
+//Description: 3 part exercise from chapter one of "Think Java"
+
+/*Exercise 1
+-In computer jargon the difference between a statement and a comment is as follows:  
+  	A statement is part of a program that specifies one step of an algorithm 
+  	and a comment is a part of a program that contains information about the program but
+  	 has no effect when the program runs. 
+-When a program is portable it is said to be able to run 
+	on more than one kind of computer.
+-In common english the word “compile” means to take a program from
+ 	an “easy to understand” programming language to an “easy to run” programming language
+  	so that it can be used at a later time.
+-An “executable” is a term defined as: Another name for object code that is ready
+ 	to run on specific hardware.”  It is a noun because it refers to an object which is ready to be
+ 	 “executed” or ready to perform a task.
+
+Think Java. (n.d.). Retrieved from https://books.trinket.io/thinkjava/chapter1.html#.
+
+ */
+
+
+//Exercise 2
+public class ChapterOneExercises {
+	
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+	System.out.println("How are you?");
+	System.out.println("I am fine, thanks");
+	}
+}
+
+
+/*
+Exercise 3: Resulting Errors From exercise three
+1. Exception in thread "main" java.lang.Error: Unresolved compilation problem
+2. Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+	Syntax error, insert "}" to complete ClassBody
+3. Main method not found in class ChapterOneExercises, please define the main method as:
+   public static void main(String[] args)
+or a JavaFX application class must extend javafx.application.Application
+4. Main method is not static in class ChapterOneExercises, please define the main method as:
+   public static void main(String[] args)
+5. Main method not found in class ChapterOneExercises, please define the main method as:
+   public static void main(String[] args)
+   or a JavaFX application class must extend javafx.application.Application
+6. Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
+	Syntax error on token ".", delete this token
+	out cannot be resolved
+7. Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
+	The method Println(String) is undefined for the type PrintStream
+8. Hello WorldHow are you?
+   I am fine, thanks
+9. Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
+	Syntax error on token "println", ( expected after this token
+	Syntax error on token ")", delete this token
+
+*/
+/*
+Footer
+Hello World
+How are you?
+I am fine, thanks
+
+
+*/
+
+
+
+	
+	
+	
+
+
